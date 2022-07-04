80ade12b7cdd2e0cd8cc75d4a5d1c28a58fae866
Stalin Alexander <50262159+astalin28@users.noreply.github.com>
2022-07-04 12:37:30 +0530

    eyedog.sh

--- eyedog.sh
+++ eyedog.sh
@@ -8,15 +8,14 @@ condition(){
 }
 
 echo -e "\033[1;31m"
+file_2=$(cat List.txt)
 file=$(cat logo.txt)
 echo -e "\033[1;31m"
 echo "$file"
 echo -e "\n \n"
-echo -e "\033[1;32m[coded by stalin]\n"
-echo "[1] instagram hack"
-echo "[2] auto admin whatsapp"
-echo "[3] number report"
-echo -e "[4] info \n\033[1;33m"
+echo -e "\033[1;32m"
+echo "$file_2"
+echo -e "\n\033[1;33m"
 
 read -p "Enter Your option: " red
 
